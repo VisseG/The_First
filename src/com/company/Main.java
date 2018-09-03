9a78d5df20033d9439e6cba77f782b46d7f155d6
VisseG <GITHUB rolexx2005@yandex.ru>
2018-09-03 20:50:12 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //System.out.println("Information about the student:\nName:\t Vitaliy Misichenko\nGroup:\t AT-161\nCountry: Ukraine\nE-mail:\t rolexx2005@yandex.ua\n");
+
+        System.out.print("Information about the student:\n" +
+                "Name:\t Vitaliy Misichenko\n" +
+                "Group:\t AT-161\n" +
+                "Country: Ukraine\n" +
+                "E-mail:\t rolexx2005@yandex.ua\n");
+
+    }
+}
